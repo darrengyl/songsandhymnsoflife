62c14e12c8d489559dbdca87f175bd2adda8712b
Darren Gu <yuanlong.gu@vaporstream.com>
2016-05-10 18:41:33 -0500

    Add png files

---
+++ app/src/main/java/com/church/psalm/model/SongMatch.java
@@ -0,0 +1,16 @@
+package com.church.psalm.model;
+
+/**
+ * Created by darrengu on 5/10/16.
+ */
+public class SongMatch {
+    private String keyword;
+    private int start;
+    private int end;
+    private String partialLyrics;
+    private Song song;
+
+    public SongMatch() {
+
+    }
+}
